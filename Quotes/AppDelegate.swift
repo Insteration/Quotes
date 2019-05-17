932cf90f9ee65760e349b728dcd68023f4c05421
Artem <inste@icloud.com>
2019-05-17 14:36:52 +0300

    First commit

--- Quotes/AppDelegate.swift
+++ Quotes/AppDelegate.swift
@@ -12,15 +12,38 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
 
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named: NSImage.Name("StatusBarButtonImage"))
+            button.action = #selector(printQuote(_:))
+        }
+        
+        constructMenu()
+
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+
     }
 
+    @objc func printQuote(_ sender: Any?) {
+        let quoteText = "Never put off until tomorrow what you can do the day after tommorow."
+        let quoteAuthor = "Mark Twain"
+        
+        print("\(quoteText) - \(quoteAuthor)")
+    }
+    
+    func constructMenu() {
+        let menu = NSMenu()
+        
+        menu.addItem(NSMenuItem(title: "Print Quote", action: #selector(printQuote(_:)), keyEquivalent: "P"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(withTitle: "Quit Quotes", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
+        
+        statusItem.menu = menu
+    }
 
 }
 
